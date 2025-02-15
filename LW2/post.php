5965342ad48289c103ad091cc8734af36da37678
V1DeeK <97947424+V1DeeK@users.noreply.github.com>
2025-02-15 20:23:18 +0300

    добавил 2 лабу

---
+++ LW2/post.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<html lang = "ru">
+<head>
+    <title>Лабораторная работа №1 Глазырин В.Д.</title>
+    </head>
+<body>
+    <header>
+        <h1>Глазырин Вадим</h1>
+    </header>
+    <hr>
+    <p> <strong>Я обучаюсь в Институте Программных Систем <img src="me.jpg" alt = ""></strong></p>
+    <article>
+    
+    <h2>
+        <Ul><strong>О Себе</strong>:
+                    <li><a href="http://www.vk.com/id=mr_varder"> Мой ВК</a></li> 
+                    <li>26 лет</li>
+			  <li>Женат</li>
+			  <li>Воспитываю собаку Боню и кошку Люсю</li>
+    </p1>
+    
+    </article>
+    <footer>
+         <p>Copyright &copy;Frontend 2025</p>
+    </footer>
+</body>
+</html>
